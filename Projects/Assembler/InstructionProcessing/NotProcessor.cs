77dbb7eabcd57f200f35ea3a2d34e6507a64729b
NoiselessLeg <7327295+NoiselessLeg@users.noreply.github.com>
2018-04-22 16:37:55 -0400

    Added not command, changed cmd line executable name to make it more friendly for keyboards

---
+++ Projects/Assembler/InstructionProcessing/NotProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembler.InstructionProcessing
+{
+    class NotProcessor : BaseInstructionProcessor
+    {
+        /// <summary>
+        /// Parses an instruction and generates the binary code for it.
+        /// </summary>
+        /// <param name="address">The address of the instruction being parsed in the .text segment.</param>
+        /// <param name="args">An array containing the arguments of the instruction.</param>
+        /// <returns>One or more 32-bit integers representing this instruction. If this interface is implemented
+        /// for a pseudo-instruction, this may return more than one instruction value.</returns>
+        public override IEnumerable<int> GenerateCodeForInstruction(int address, string[] args)
+        {
+            // we expect two arguments. if not, throw an ArgumentException
+            if (args.Length != 2)
+            {
+                throw new ArgumentException("Invalid number of arguments provided. Expected 2, received " + args.Length + '.');
+            }
+
+            return new XoriProcessor().GenerateCodeForInstruction(address, new string[] { args[0], args[1], "-1" });
+        }
+    }
+}
